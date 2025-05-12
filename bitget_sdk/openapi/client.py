4fa77c9bbe25fc2c0fb0cc04af9601ff62d83838
kentinou <161424467+kentinou@users.noreply.github.com>
2025-05-12 14:19:53 +0200

    Delete bitget_sdk/openapi directory

--- bitget_sdk/openapi/client.py
+++
@@ -1,5 +0,0 @@
-# Simulated Bitget Client for local import
-class Client:
-    def __init__(self, api_key, api_secret, passphrase, use_server_time=True): pass
-    def mix_place_order(self, **kwargs): return {'result': 'simulated_order'}
-    def mix_get_account(self, symbol, marginCoin): return {'data': {'available': '100'}}
